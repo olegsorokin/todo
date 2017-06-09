3d0de1524bfd52ae1b6b0270d42a8241ec1fc79f
olegsorokin <style-x55@mail.ru>
2017-06-09 19:31:24 +0600

    Добавил GET/JSON

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ const app = express();
 
 const tasks = [];
 
+const generatedId = (arr) => arr.length + 1;
+
 app.post('/tasks', (req, res) => {
   res.send('Hello World!');
 });
@@ -12,9 +14,14 @@ app.get('/tasks', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/tasks/:id/:name/:time', (req, res) => {
+app.get('/tasks/:id/:name/:time/:descr', (req, res) => {
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({ name: 'req.name' }));
+  res.send(JSON.stringify({
+    id: req.params.id,
+    name: req.params.name,
+    time: req.params.time,
+    descr: req.params.descr,
+  }));
 });
 
 app.put('/tasks/:id', (req, res) => {
